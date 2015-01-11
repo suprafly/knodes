e6b995ded431c28e26f7aad742f5290f61a1bac3
suprafly <info@suprafly.com>
2015-01-11 21:37:18 +0530

    Refactor Some Functions

--- knodes.py
+++ knodes.py
@@ -6,6 +6,9 @@ from config import DEBUG_MODE
 
 # Helper functions
 remove_commas = lambda string: string.replace(",", " ")
+has_one_or_more_tags = lambda tags: tags.__len__() < 1
+has_knowledge_node = lambda text: text.lstrip().__len__() < 1
+create_tag_list = lambda tag_text: DB.sanitize_tags(tag_text.split(" "))
 
 
 app = Flask(__name__)
@@ -21,22 +24,16 @@ def index_post():
         knode_id = request.form['knode_id']
         title = request.form['title']
         text = request.form['text']
-        tag_text = remove_commas(request.form['tags'])
-        tags = DB.sanitize_tags(tag_text.split(" "))
-
-        #! Don't let them enter 0 tags, or else the knode disappears!
-        #! Don't let them enter without a Knowledge Node!
-        if (tags.__len__() < 1) or (text.lstrip().__len__() < 1):
+        tags = create_tag_list(remove_commas(request.form['tags']))
+        if has_one_or_more_tags(tags) or has_knowledge_node(text):
             knode = DB.get_knode(knode_id)
             return render_template('edit.html', title=knode[0], text=knode[1], tags=knode[2], knode_id=knode_id)
         else:
             DB.update_knode(knode_id, title, text, tags)
-
     elif 'delete_button' in request.form:
         knode_id = request.form['knode_id']
         DB.remove_knodefile(DB.get_node_xml_file(knode_id))
-        tag_text = remove_commas(request.form['tags'])
-        tags = DB.sanitize_tags(tag_text.split(" "))
+        tags = create_tag_list(remove_commas(request.form['tags']))
         DB.remove_knode_id_from_tagfiles(knode_id, tags)
     else:
         text = request.form['knode']
@@ -47,12 +44,9 @@ def index_post():
         else:
             title = ""
             text = text.lstrip()
-
-        tag_text = remove_commas(request.form['tags'])
-        tags = DB.sanitize_tags(tag_text.split(" "))
+        tags = create_tag_list(remove_commas(request.form['tags']))
         knode_id = DB.create_knode(title, text, tags)
         DB.save_tags(tags, knode_id)
-
     all_tags = DB.get_all_tags()
     tags_with_knodes = DB.get_knodes_for_tags(all_tags)
     return render_template("layout.html", tags_with_knodes=tags_with_knodes)
